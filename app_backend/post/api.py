cbb444c86350d3868dcf0354bba093593f1bf8b1
Abu Ubaida <ubaidanazia@gmail.com>
2024-08-24 00:53:48 +0600

    Post creation from frontend

--- app_backend/post/api.py
+++ app_backend/post/api.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 
 from .models import Post
+from .forms import PostForm
 from .serializers import PostSerializer
 
 @api_view(['GET'])
@@ -11,3 +12,15 @@ def post_list(request):
     serializer = PostSerializer(posts, many=True)
 
     return JsonResponse(serializer.data, safe=False)
+
+
+@api_view(['POST'])
+def post_create(request):
+        form = PostForm(request.data)
+
+        if form.is_valid():
+              post = form.save(commit=False)
+              post.created_by = request.user
+              post.save()
+
+        return JsonResponse({'hello':'hy'})
