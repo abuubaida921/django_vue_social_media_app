3b040e384869cb9986fbe0ee17e3e79e510732f2
Abu Ubaida <ubaidanazia@gmail.com>
2024-08-23 10:45:52 +0600

    Django CSRF and CORS setup done

--- app_backend/app_backend/settings.py
+++ app_backend/app_backend/settings.py
@@ -48,6 +48,14 @@ REST_FRAMEWORK = {
     )
 }
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5173",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.0.1:5173",
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -57,11 +65,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'djangorestframework',
     'djangorestframework_simplejwt',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
